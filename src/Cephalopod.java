a95f25d36072870999e9e97c73efe8fce021f4e3
HaroldBalinagay <harold_balinagay@dlsu.edu.ph>
2026-06-17 23:32:56 +0800

    Created Cephalopod abstract class

--- src/Cephalopod.java
+++ src/Cephalopod.java
@@ -1,3 +1,13 @@
 public abstract class Cephalopod {
-
+    private int tentacleCount;
+    public Cephalopod(int tentacleCount){
+        this.tentacleCount = tentacleCount;
+    }
+    public int getTentacleCount() {
+        return tentacleCount;
+    }
+    public void propel() {
+        IO.println("The cephalopod uses its tentacles to propel itself");
+    }
+    public abstract void defense();
 }
