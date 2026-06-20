b8f0fb9795aae493abccf552bbe86b7d6b343a5e
HaroldBalinagay <harold_balinagay@dlsu.edu.ph>
2026-06-20 21:22:31 +0800

    Created tiger shark class

---
+++ src/TigerShark.java
@@ -0,0 +1,20 @@
+public class TigerShark extends Chondrichthyes {
+    private int stripeCount;
+    public TigerShark(String typeOfHabitat, int stripeCount) {
+        super(typeOfHabitat);
+        setStripeCount(stripeCount);
+    }
+    public int getStripeCount() {
+        return stripeCount;
+    }
+    public void setStripeCount(int stripeCount) {
+        this.stripeCount = stripeCount;
+    }
+    public void detectElectricity() {
+        IO.println("The tiger shark senses prey using electroreceptors in its snout.");
+    }
+    @Override
+    public void swim() {
+        IO.println("Swims quickly through coastal waters looking for food.");
+    }
+}
